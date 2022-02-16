475e7bc254db0b496e5c193e31284ea2f4a3caeb
Usu2 Name <usu2@ejemplo.com>
2022-02-16 02:39:40 +0100

    Valido la clase principal momento1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,5 +3,8 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas git. se añade cambio1");
 	}
 }
